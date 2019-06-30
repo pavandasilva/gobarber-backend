cd825953b200c7feaef96f0de24d1d6d50028b6a
Rogerio Pavan da Silva <rogerio@MacBook-Pro-de-Rogerio.local>
2019-06-30 16:34:14 -0300

    :rocket: feature/list-provider lista prestadores de serviços
    cria controller ProviderController
    
    cria index em ProviderController
    
    altera o model File.js para retornar a url completa do arquivo
    
    criar rota /providers em routes.js

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -16,5 +17,6 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
+routes.get('/providers', ProviderController.index);
 
 export default routes;
