dc043b67b418cbd86c3fcc9ccf732c16aedb4db9
Rogerio Pavan da Silva <rogerio@MacBook-Pro-de-Rogerio.local>
2019-07-01 01:44:42 -0300

    :wrench: chore/email-config configura o envio de email
    cria a classe Mail em lib
    
    cria função de enviar email na classe Mail
    
    cria configuração de email em config/mail
    
    instala o modulo nodemailer

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -5,6 +5,7 @@ import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
+import Mail from '../../lib/Mail';
 
 class AppointmentController {
   async index(req, res) {
@@ -113,7 +114,15 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+      ],
+    });
 
     if (!appointment) {
       return res.status(400).json({ error: 'Appointment does not exist' });
@@ -135,6 +144,12 @@ class AppointmentController {
 
     appointment.canceled_at = new Date();
     await appointment.save();
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      text: 'Você tem um novo cancelamento',
+    });
 
     return res.json(appointment);
   }
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: `smtp.mailtrap.io`,
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '578b245b55f5f0',
+    pass: '42907fc92abb06',
+  },
+  default: {
+    from: 'GoBarber <noreply@gobarber.com>',
+  },
+};
