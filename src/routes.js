00d66689de050c03faba80581eba8593a8566585
Rogerio Pavan da Silva <rogerio@MacBook-Pro-de-Rogerio.local>
2019-08-04 11:16:12 -0300

    :art: chore/safety
    instala cors, express-brute, express-brute-redis, express-rate-limite helmet
    
    adiciona o brute na roda POST/sessions em routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
+import Brute from 'express-brute';
+import BruteRedis from 'express-brute-redis';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
@@ -19,8 +21,20 @@ import validadeAppointmentStore from './app/validators/AppointmentStore';
 const routes = new Router();
 const upload = multer(multerConfig);
 
+const bruteStore = new BruteRedis({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+});
+
+const bruteForce = new Brute(bruteStore);
+
 routes.post('/users', validadeUserStore, UserController.store);
-routes.post('/sessions', validadeSessionStore, SessionController.store);
+routes.post(
+  '/sessions',
+  bruteForce.prevent,
+  validadeSessionStore,
+  SessionController.store
+);
 
 routes.use(authMiddleware);
 
